890b6545c2d96e37470bf16d25d36966077997b2
Ishaque Anas Shaikh <anasshaikh129229@gmail.com>
2025-10-13 01:15:20 +0530

    Final cleanup: Remove venv and large files from Git history

--- evaluator.py
+++
@@ -1,9 +0,0 @@
-import nltk
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-
-def evaluate_prompt(prompt):
-    words = prompt.split()
-    stopword_ratio = sum(1 for w in words if w in stopwords.words('english')) / len(words)
-    clarity = round((1 - stopword_ratio) * 10, 2)
-    return {"clarity_score": clarity}
